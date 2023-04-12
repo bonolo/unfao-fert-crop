4f8f6e77f51e7beaea3157cd64822f19fe7e7a88
Kevin Cullen <bonolo@users.noreply.github.com>
2023-04-12 12:17:39 -0600

    Agave fixes

--- grep-product-csv.sh
+++ grep-product-csv.sh
@@ -20,10 +20,20 @@ grep -irh "Republic of Korea" crops2017-21.csv >> apac-countries.csv
 grep -irh "Malaysia" crops2017-21.csv >> apac-countries.csv
 grep -irh "Australia" crops2017-21.csv >> apac-countries.csv
 
-head -1 Production_Crops_Livestock_E_All_Data_\(Normalized\).csv > production_crops_expurgated.csv
-cat agave2021.csv apac-countries.csv >> production_crops_expurgated.csv
+# remove agave rows from apac
+grep -irhv "agave" apac-countries.csv > apac-no-agave.csv
 
-rm apac-countries.csv
+# column headers
+head -1 Production_Crops_Livestock_E_All_Data_\(Normalized\).csv > production_crops_expurgated.csv
+# put everything into smaller file
+cat agave2021.csv apac-no-agave.csv >> production_crops_expurgated.csv
+
+# remove temporary files
+rm crops2017-21.csv
 rm agave.csv
 rm agave2021.csv
-rm crops2017-21.csv
+rm apac-countries.csv
+rm apac-no-agave.csv
+
+
+# xattr -d com.apple.quarantine ./grep-product-csv.sh
